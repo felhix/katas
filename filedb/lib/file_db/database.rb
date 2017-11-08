668508e60d6719beaddf88ac656f908e4326d61f
felhix <felhix@hotmail.com>
2017-11-08 19:52:17 +0100

    premiers tests effectués

--- filedb/lib/file_db/database.rb
+++ filedb/lib/file_db/database.rb
@@ -1,6 +1,23 @@
 module FileDb
+  require 'json'
+
   class Database
+      attr_accessor :hash
+
     def initialize(data_file)
+      @hash = path_to_hash(data_file)
     end
+
+    def table_names
+      @hash.keys.sort
+    end
+
+    private
+
+      def path_to_hash(path)
+        file = File.read(path)
+        hash = JSON.parse(file)
+      end
+
   end
 end
